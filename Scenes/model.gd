505e1cf5c0a8738f7c7c72f0d4428f1ac3069b19
Jelle van 't Veer <jellevtveer@gmail.com>
2024-01-28 11:29:28 +0100

    Add a guidance line and change color for small dot when it is first instanciated.

--- Scenes/model.gd
+++ Scenes/model.gd
@@ -8,6 +8,9 @@ var old_pos:Vector2
 @export var ccspt:bool = false
 var last_main_dot = null
 var pause:bool = false
+var guidance_line:bool = false
+@onready var line:Line2D = $Line
+var last_small_dot:Node2D
 
 #TODO: sim speed
 #TODO: slider for smaller dot size
@@ -21,7 +24,8 @@ func _ready():
 	randomize()
 	
 	r = 1-(main_dot_amount/(main_dot_amount+3.0))
-	reset(r, main_dot_amount)
+	reset(main_dot_amount)
+	line.top_level = true
 
 #endregion
 #region Process
@@ -51,33 +55,53 @@ func new_small_dot():
 		
 		var target_location:Vector2 = (old_pos+random_main_dot.position)*r # cannot find a random dot
 		
+		if last_small_dot != null:
+			last_small_dot.scale *= 0.6
+			var locations = []
+			for main_dot in main_dots:
+				locations.append(main_dot.position.distance_to(last_small_dot.position))
+		
+			var closest_main_dot = locations.find(locations.min())
+
+			var h =(1.0+closest_main_dot)/main_dot_amount
+			last_small_dot.modulate = Color.from_hsv(h,0.4,1)
+		
 		var small_dot = preload("res://Scenes/smaller_dot.tscn").instantiate()
 		add_child(small_dot)
 		small_dot.top_level = true
 		
 		small_dot.position = target_location
 		
-		var locations:Array
-		for main_dot in main_dots:
-			locations.append(main_dot.position.distance_to(small_dot.position))
 		
-		var closest_main_dot = locations.find(locations.min())
-
-		var h =(1.0+closest_main_dot)/main_dot_amount
-		small_dot.modulate = Color.from_hsv(h,0.4,1)
+		if guidance_line:
+			line.set_point_position(0, random_main_dot.position)
+			if last_main_dot == null:
+				line.set_point_position(1, start_position)
+			else:
+				line.set_point_position(1, old_pos)
+		
+		small_dot.scale *= 1.4
+		small_dot.modulate = Color.from_hsv(0,0,0)
+		
+		last_small_dot = small_dot
 		
 		old_pos = target_location
 		last_main_dot = random_main_dot_index
 
 #endregion
 #region Reseting
-func reset(r_local, main_dot_amount_local):
+func reset(main_dot_amount_local):
 	randomize()
 	var viewport_size = get_viewport_rect().size
 	var radius = viewport_size.y*2
 	
+	var regex = RegEx.new()
+	regex.compile(".*CanvasModulate.*|MainDot.*")
+	
 	for dot in get_children():
-		dot.free()
+		if regex.search(dot.name):
+			dot.free()
+	print(get_children())
 	main_dots = []
 	
 	var name_scheme = "MainDot%s" # cool gimick https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_format_string.html
@@ -95,6 +119,8 @@ func reset(r_local, main_dot_amount_local):
 	for dot in get_children():
 		print("---\nName:%s\nPosition:%s" % [dot.name, dot.position])
 	print("---")
+	
+	reset_line()
 	
 	print("\n--- Value of r: " + str(r) + "---")
 	
@@ -117,7 +143,7 @@ func _on_recalculate_r_pressed():
 	r = 1-(main_dot_amount/(main_dot_amount+3.0))
 
 func _on_reset_button_pressed():
-	reset(r, main_dot_amount)
+	reset(main_dot_amount)
 
 func _on_ccspt_button_toggled(button_pressed):
 	ccspt = button_pressed
@@ -127,4 +153,13 @@ func _on_pause_button_pressed():
 
 func _on_step_button_pressed():
 	new_small_dot()
+
+func _on_line_button_toggled(toggled_on):
+	guidance_line = toggled_on
+	if not toggled_on:
+		reset_line()
+		
 #endregion
+func reset_line():
+	line.set_point_position(0,start_position)
+	line.set_point_position(1, start_position)
